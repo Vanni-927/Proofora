fba8d64464d4c68ab00dad1235a858ad5f7c8761
Rijul Rangta <rijulrangta@gmail.com>
2025-10-31 09:40:49 +0530

    Initial Commit

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    host: "0.0.0.0", // works better cross-platform than "::"
+    port: 8080,
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+});
